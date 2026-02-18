7872b6c175a8a828ecdc67f55574f6158d890eb4
kanikaa678 <kanika.a662007@gmail.com>
2026-02-17 20:35:34 -0800

    Add files via upload

---
+++ Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Post() {
+  return (
+    <div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         Quia ratione magnam perferendis voluptatem, in tenetur architecto eaque vero dolore dicta consequuntur enim excepturi adipisci ipsam perspiciatis officiis laudantium laborum aperiam sapiente aliquam, quidem quasi quas.
+         Ducimus expedita asperiores cum veniam.</p>
+    </div>
+  )
+}
+
+export default Post
